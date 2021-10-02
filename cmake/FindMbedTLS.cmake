688634e55502e6f33dbf6ca2ca57071de19635b5
Roman Senn <roman.l.senn@gmail.com>
2021-10-03 01:15:16 +0200

    ...

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -10,15 +10,23 @@ macro(find_mbedtls)
 
   # based on https://github.com/ARMmbed/mbedtls/issues/298
   if(MBEDTLS_INCLUDE_DIR AND EXISTS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h")
-    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_STRING_LINE REGEX "^#define MBEDTLS_VERSION_STRING[ \\t\\n\\r]+\"[^\"]*\"$")
-    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_MAJOR_LINE REGEX "^#define MBEDTLS_VERSION_MAJOR[ \\t\\n\\r]+[0-9]+$")
-    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_MINOR_LINE REGEX "^#define MBEDTLS_VERSION_MINOR[ \\t\\n\\r]+[0-9]+$")
-    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_PATCH_LINE REGEX "^#define MBEDTLS_VERSION_PATCH[ \\t\\n\\r]+[0-9]+$")
+    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_STRING_LINE
+         REGEX "^#define MBEDTLS_VERSION_STRING[ \\t\\n\\r]+\"[^\"]*\"$")
+    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_MAJOR_LINE
+         REGEX "^#define MBEDTLS_VERSION_MAJOR[ \\t\\n\\r]+[0-9]+$")
+    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_MINOR_LINE
+         REGEX "^#define MBEDTLS_VERSION_MINOR[ \\t\\n\\r]+[0-9]+$")
+    file(STRINGS "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" VERSION_PATCH_LINE
+         REGEX "^#define MBEDTLS_VERSION_PATCH[ \\t\\n\\r]+[0-9]+$")
 
-    string(REGEX REPLACE "^#define MBEDTLS_VERSION_STRING[ \\t\\n\\r]+\"([^\"]*)\"$" "\\1" MBEDTLS_VERSION "${VERSION_STRING_LINE}")
-    string(REGEX REPLACE "^#define MBEDTLS_VERSION_MAJOR[ \\t\\n\\r]+([0-9]+)$" "\\1" MBEDTLS_VERSION_MAJOR "${VERSION_MAJOR_LINE}")
-    string(REGEX REPLACE "^#define MBEDTLS_VERSION_MINOR[ \\t\\n\\r]+([0-9]+)$" "\\1" MBEDTLS_VERSION_MINOR "${VERSION_MINOR_LINE}")
-    string(REGEX REPLACE "^#define MBEDTLS_VERSION_PATCH[ \\t\\n\\r]+([0-9]+)$" "\\1" MBEDTLS_VERSION_PATCH "${VERSION_PATCH_LINE}")
+    string(REGEX REPLACE "^#define MBEDTLS_VERSION_STRING[ \\t\\n\\r]+\"([^\"]*)\"$" "\\1" MBEDTLS_VERSION
+                         "${VERSION_STRING_LINE}")
+    string(REGEX REPLACE "^#define MBEDTLS_VERSION_MAJOR[ \\t\\n\\r]+([0-9]+)$" "\\1" MBEDTLS_VERSION_MAJOR
+                         "${VERSION_MAJOR_LINE}")
+    string(REGEX REPLACE "^#define MBEDTLS_VERSION_MINOR[ \\t\\n\\r]+([0-9]+)$" "\\1" MBEDTLS_VERSION_MINOR
+                         "${VERSION_MINOR_LINE}")
+    string(REGEX REPLACE "^#define MBEDTLS_VERSION_PATCH[ \\t\\n\\r]+([0-9]+)$" "\\1" MBEDTLS_VERSION_PATCH
+                         "${VERSION_PATCH_LINE}")
   endif()
 
   if(MBEDTLS_USE_STATIC_LIBS)
@@ -31,9 +39,12 @@ macro(find_mbedtls)
     set(_MBEDTLS_X509_LIB_NAME mbedx509)
   endif()
 
-  find_library(MBEDTLS_LIBRARY NAMES ${_MBEDTLS_LIB_NAME} PATH_SUFFIXES lib HINTS ${MBEDTLS_ROOT_DIR} ${_EXTRA_FIND_ARGS})
-  find_library(MBEDTLS_CRYPTO_LIBRARY NAMES ${_MBEDTLS_CRYPTO_LIB_NAME} PATH_SUFFIXES lib HINTS ${MBEDTLS_ROOT_DIR} ${_EXTRA_FIND_ARGS})
-  find_library(MBEDTLS_X509_LIBRARY NAMES ${_MBEDTLS_X509_LIB_NAME} PATH_SUFFIXES lib HINTS ${MBEDTLS_ROOT_DIR} ${_EXTRA_FIND_ARGS})
+  find_library(MBEDTLS_LIBRARY NAMES ${_MBEDTLS_LIB_NAME} PATH_SUFFIXES lib HINTS ${MBEDTLS_ROOT_DIR}
+                                                                                  ${_EXTRA_FIND_ARGS})
+  find_library(MBEDTLS_CRYPTO_LIBRARY NAMES ${_MBEDTLS_CRYPTO_LIB_NAME} PATH_SUFFIXES lib HINTS ${MBEDTLS_ROOT_DIR}
+                                                                                                ${_EXTRA_FIND_ARGS})
+  find_library(MBEDTLS_X509_LIBRARY NAMES ${_MBEDTLS_X509_LIB_NAME} PATH_SUFFIXES lib HINTS ${MBEDTLS_ROOT_DIR}
+                                                                                            ${_EXTRA_FIND_ARGS})
 
   set(MBEDTLS_LIBRARIES ${MBEDTLS_LIBRARY} ${MBEDTLS_CRYPTO_LIBRARY} ${MBEDTLS_X509_LIBRARY})
 
@@ -42,24 +53,34 @@ macro(find_mbedtls)
   endif(MBEDTLS_INCLUDE_DIR AND MBEDTLS_LIBRARY)
 
   include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(mbedTLS FOUND_VAR MBEDTLS_FOUND REQUIRED_VARS MBEDTLS_INCLUDE_DIR MBEDTLS_LIBRARY MBEDTLS_CRYPTO_LIBRARY MBEDTLS_X509_LIBRARY MBEDTLS_LIBRARIES MBEDTLS_VERSION VERSION_VAR MBEDTLS_VERSION)
+  find_package_handle_standard_args(
+    mbedTLS FOUND_VAR MBEDTLS_FOUND REQUIRED_VARS MBEDTLS_INCLUDE_DIR MBEDTLS_LIBRARY MBEDTLS_CRYPTO_LIBRARY
+                                                  MBEDTLS_X509_LIBRARY MBEDTLS_LIBRARIES MBEDTLS_VERSION
+    VERSION_VAR MBEDTLS_VERSION)
 
   message("MBEDTLS_FOUND: ${MBEDTLS_FOUND}")
 
   if(MBEDTLS_FOUND)
     if(NOT TARGET mbedcrypto)
       add_library(mbedcrypto UNKNOWN IMPORTED)
-      set_target_properties(mbedcrypto PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIR}" IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${MBEDTLS_CRYPTO_LIBRARY}")
+      set_target_properties(
+        mbedcrypto
+        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIR}" IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                   IMPORTED_LOCATION "${MBEDTLS_CRYPTO_LIBRARY}")
     endif()
 
     if(NOT TARGET mbedx509)
       add_library(mbedx509 UNKNOWN IMPORTED)
-      set_target_properties(mbedx509 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIR}" INTERFACE_LINK_LIBRARIES mbedcrypto IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${MBEDTLS_X509_LIBRARY}")
+      set_target_properties(
+        mbedx509 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIR}" INTERFACE_LINK_LIBRARIES mbedcrypto
+                            IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${MBEDTLS_X509_LIBRARY}")
     endif()
 
     if(NOT TARGET mbedtls)
       add_library(mbedtls UNKNOWN IMPORTED)
-      set_target_properties(mbedtls PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIR}" INTERFACE_LINK_LIBRARIES mbedx509 IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${MBEDTLS_LIBRARY}")
+      set_target_properties(
+        mbedtls PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIR}" INTERFACE_LINK_LIBRARIES mbedx509
+                           IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${MBEDTLS_LIBRARY}")
     endif()
 
     get_target_property(MBEDTLS_LIBRARY_LOCATION mbedtls IMPORTED_LOCATION)
