9de719868212c21a6b2e42597f56c2e42c98c655
Roman Senn <roman.l.senn@gmail.com>
2022-03-07 18:16:57 +0100

    ...

--- minnet-server.c
+++ minnet-server.c
@@ -21,19 +21,19 @@ int http_server_callback(struct lws*, enum lws_callback_reasons, void*, void*, s
 
 static struct lws_protocols protocols[] = {
     {"ws", ws_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
-    {"defprot", lws_callback_http_dummy, 0, 0},
+    {"defprot", lws_callback_http_dummy, sizeof(MinnetSession), 0},
     {"http", http_server_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
     // {"proxy-ws", proxy_callback, 0, 1024, 0, NULL, 0},
-    {"proxy-raw", raw_client_callback, 0, 1024, 0, NULL, 0},
+    {"proxy-raw", raw_client_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
     {0},
 };
 
 static struct lws_protocols protocols2[] = {
     {"ws", ws_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
-    {"defprot", defprot_callback, 0, 0},
+    {"defprot", defprot_callback, sizeof(MinnetSession), 0},
     {"http", http_server_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
-    {"proxy-ws", proxy_callback, 0, 1024, 0, NULL, 0},
-    {"proxy-raw", raw_client_callback, 0, 1024, 0, NULL, 0},
+    {"proxy-ws", proxy_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
+    {"proxy-raw", raw_client_callback, sizeof(MinnetSession), 1024, 0, NULL, 0},
     {0, 0},
 };
 
