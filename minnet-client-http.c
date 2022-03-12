55a0b31d4e549693db28a64ca0981cf8f5a688fb
Roman Senn <roman.l.senn@gmail.com>
2022-03-12 10:55:45 +0100

    ...

--- minnet-client-http.c
+++ minnet-client-http.c
@@ -107,9 +107,10 @@ http_client_callback(struct lws* wsi, enum lws_callback_reasons reason, void* us
       lwsl_user("http-established #1 " FGC(171, "%-38s") "  server response: %d\n", lws_callback_name(reason) + 13, status);
       // session->req_obj = minnet_request_wrap(ctx, client->request);
 
-      session->resp_obj = minnet_response_new(ctx, url_clone(client->request->url, ctx), status, TRUE, "text/html");
+      session->resp_obj = minnet_response_wrap(ctx, client->response);
+      // session->resp_obj = minnet_response_new(ctx, url_clone(client->request->url, ctx), status, TRUE, "text/html");
 
-      client->response = minnet_response_data(session->resp_obj);
+      //  client->response = minnet_response_data(session->resp_obj);
 
       if(method_number(client->connect_info.method) == METHOD_POST) {
         lws_client_http_body_pending(wsi, 1);
