b105216cfd5acde85088d4dce82cda31ffd41fab
Roman Senn <roman.l.senn@gmail.com>
2022-12-04 22:14:47 +0100

    ...

--- lib/deferred.c
+++ lib/deferred.c
@@ -1,7 +1,21 @@
 #include "deferred.h"
 
 void
-deferred_init(Deferred* def) {
+deferred_clear(Deferred* def) {
   def->func = 0;
-  memset(def->args, 0, sizeof(def->args));
+  for(int i = 0; i < 8; i++) def->args[i] = 0;
+}
+
+void
+deferred_init(Deferred* def, void* fn, int argc, void* argv[]) {
+  int i;
+  def->func = fn;
+
+  for(i = 0; i < argc; i++) def->args[i] = argv[i];
+  for(; i < 8; i++) def->args[i] = 0;
+}
+
+void*
+deferred_call(const Deferred* def) {
+  return def->func(def->args[0], def->args[1], def->args[2], def->args[2], def->args[4], def->args[5], def->args[6], def->args[7]);
 }
--- lib/deferred.h
+++ lib/deferred.h
@@ -1,1 +1,13 @@
-#include "deferred.h"
+#ifndef QUICKJS_NET_LIB_DEFERRED_H
+#define QUICKJS_NET_LIB_DEFERRED_H
+
+typedef struct deferred {
+  void* (*func)(void*, void*, void*, void*, void*, void*, void*, void*);
+  void* args[8];
+} Deferred;
+
+void deferred_clear(Deferred*);
+void deferred_init(Deferred*, void* fn, int argc, void* argv[]);
+void* deferred_call(const Deferred*);
+
+#endif /* QUICKJS_NET_LIB_DEFERRED_H */
