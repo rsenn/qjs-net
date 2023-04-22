f2def7108e552e90e8c9a30c30e61bafa5608709
Roman Senn <roman.l.senn@gmail.com>
2023-04-22 04:32:15 +0200

    ...

--- examples/http-client.js
+++ examples/http-client.js
@@ -1,12 +1,13 @@
 import net from 'net';
 
-//net.setLog(net.LLL_USER, (level, message) => (true ? console.log('minnet', message.replace(/\n/g, '\\n')) : undefined));
+// net.setLog(net.LLL_USER, (level, message) => (true ? console.log('minnet', message.replace(/\n/g, '\\n')) : undefined));
 
 function request(url) {
   return net.client(url, {
     block: false,
     async onResponse(ws, resp) {
       console.log('Response for', resp.url);
+
       let chunks = [];
       for await(let data of resp.body) {
         chunks.push(data);
@@ -21,8 +22,8 @@ function request(url) {
     onClose(ws, ...args) {
       console.log('closed', ...args);
     }
-  });
+  }).then(client => console.log('client.socket.fd',client.socket.fd));
 }
 
-await request('https://github.com/rsenn?tab=repositories');
+request('https://github.com/rsenn?tab=repositories');
 await request('https://github.com/topics/http2');
