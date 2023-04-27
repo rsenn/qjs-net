440b4909794b77c35ebc045f7eed435129b1ef14
Roman Senn <roman.l.senn@gmail.com>
2023-04-27 21:18:22 +0200

    ...

--- examples/http-server.js
+++ examples/http-server.js
@@ -14,6 +14,6 @@ createServer({
     '/': ['.', 'stream.mp3'],
     *'/404.html'(req, res) {
       yield `<html>\n\t<head>\n\t\t<meta charset=utf-8 http-equiv="Content-Language" content="en" />\n\t\t<link rel="stylesheet" type="text/css" href="/error.css" />\n\t</head>\n\t<body>\n\t\t<h1>404</h1>\n\t\tThe requested URL ${req.url.path} was not found on this server.\n\t</body>\n</html>\n`;
-    } 
+    }
   }
 });
