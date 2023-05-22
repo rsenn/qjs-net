77b6b8a6845a6fa10dde304910ae912ec9a1c415
Roman Senn <roman.l.senn@gmail.com>
2023-05-23 00:28:40 +0200

    ...

--- tests/test-generator.js
+++ tests/test-generator.js
@@ -1,38 +1,6 @@
 import { Generator } from 'net.so';
 import { fail, assert, eq, assertEquals, assertStrictEquals, tests } from './tinytest.js';
 
-import('console').then(({ Console }) => (globalThis.console = new Console({ inspectOptions: { compact: 2 } })));
-
-async function main() {
-  let gen = new Generator(async (push, stop) => {
-    console.log('generator', { push, stop });
-    for(let i = 0; i < 100; i++) {
-      let pr = push(new Uint32Array([i * 1e6]));
-      await pr;
-      console.log(`wrote #${i}`);
-
-      if(i >= 42) stop();
-    }
-    await stop(new Uint8Array([70, 105, 110, 105, 115, 104]));
-  });
-  console.log('gen', gen);
-
-  /* for await(let item of gen) {
-    console.log('item', new Uint32Array(item));
-  }*/
-
-  let item,
-    i = 0;
-
-  while((item = await gen.next())) {
-    let { value, done } = item;
-    console.log(`item #${i}`, { value, done });
-    if(done) break;
-  }
-}
-
-//main();
-
 tests({
   async 'next() value'() {
     const gen = new Generator(async (push, stop) => push(await push(undefined)));
