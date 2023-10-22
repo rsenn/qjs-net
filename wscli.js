fac3cef872f0167a7c0cf4ca444b412832053f03
Roman Senn <roman.l.senn@gmail.com>
2023-10-22 03:54:44 +0200

    ...

--- wscli.js
+++ wscli.js
@@ -1,14 +1,8 @@
-import { client } from 'net.so';
-import { createServer } from 'net.so';
-import { LLL_INFO } from 'net.so';
-import { LLL_NOTICE } from 'net.so';
-import { LLL_USER } from 'net.so';
-import { Request } from 'net.so';
-import { setLog } from 'net.so';
-import { URL } from 'net.so';
+#!/usr/bin/env qjsm
+import { client, createServer, LLL_INFO, LLL_NOTICE, LLL_USER, Request, setLog, URL } from 'net.so';
 import * as os from 'os';
 import * as std from 'std';
-#!/usr/bin/env qjsm
+
 const connections = new Set();
 
 let debug = 0,
