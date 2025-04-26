76e68d6ac07e1feba775818f89e88823c4c1a5c8
Roman Senn <roman.l.senn@gmail.com>
2025-04-26 14:45:35 +0200

    ...

--- js/reflect.js
+++ js/reflect.js
@@ -1,14 +1,14 @@
 import { inspect } from 'inspect';
 
 class JSValue {}
-/* prettier-ignore */ class JSNumber extends JSValue { constructor(value, type) { super(); if(this.value !== undefined) this.value = value; if(type) this.type = type; } }
-/* prettier-ignore */ class JSString extends JSValue { constructor(value) { super(); if(this.value !== undefined) this.value = value; } }
-/* prettier-ignore */ class JSBoolean extends JSValue { constructor(value) { super(); if(this.value !== undefined) this.value = value; } }
+/* prettier-ignore */ class JSNumber extends JSValue { constructor(value, type) { super(); if(value !== undefined) this.value = value; if(type) this.type = type; } }
+/* prettier-ignore */ class JSString extends JSValue { constructor(value) { super(); if(value !== undefined) this.value = value; } }
+/* prettier-ignore */ class JSBoolean extends JSValue { constructor(value) { super(); if(value !== undefined) this.value = value; } }
 /* prettier-ignore */ class JSSymbol extends JSValue { constructor(value, global) { super(); if(global) this.global=global;   if(value) this.value = value; } }
 /* prettier-ignore */ class JSRegExp extends JSValue { constructor(source, flags) { super(); this.source = source; this.flags = flags; } }
 /* prettier-ignore */ class JSObject extends JSValue { constructor(members, proto) { super(); if(members) this.members = members; if(proto) this.proto = proto; } }
 /* prettier-ignore */ class JSArray extends JSObject { constructor(arr) { super(arr ? [...arr] : undefined); } }
-/* prettier-ignore */ class JSFunction extends JSObject { constructor(code, members, proto) { super(members,proto); if(this.code) this.code = code; } }
+/* prettier-ignore */ class JSFunction extends JSObject { constructor(code, members, proto) { super(members,proto); if(code) this.code = code; } }
 /* prettier-ignore */ class JSProperty   { constructor(get, set) { if(get) this.get = get;  if(set) this.set = set; } }
 
 define(JSValue.prototype, { [Symbol.toStringTag]: 'JSValue' });
@@ -31,29 +31,27 @@ export function EncodeJS(val, info = Object.setPrototypeOf({}, null)) {
 
   switch (type) {
     case 'function':
-    case 'object': {
+    case 'object':
       if(type == 'object' && val === null) {
         info = new JSValue();
         info.type = 'null';
         break;
       }
-
       const isFunction = type == 'function';
       const isArray = type == 'object' && Array.isArray(val) && val !== Array.prototype;
       const isTypedArray = (Object.getPrototypeOf(val) == TypedArrayPrototype || val instanceof TypedArrayPrototype.constructor) && val !== TypedArrayPrototype;
       const isArrayBuffer = (Object.getPrototypeOf(val) == ArrayBuffer.prototype || val instanceof ArrayBuffer) && val !== ArrayBuffer.prototype;
-
-      if(isFunction) info = define(new JSFunction(), { code: /=>/.test(val + '') ? (val + '').replace(/^\((.*)\)\s*=>\s/, '($1) => ') : (val + '').replace(/^(function\s*|)(.*)/, '(function $2)') });
+      if(isFunction)
+        info = define(new JSFunction(), {
+          code: /=>/.test(val + '') ? (val + '').replace(/^\((.*)\)\s*=>\s/, '($1) => ') : (val + '').replace(/^(function\s*|)(.*)/, '(function $2)'),
+        });
       else if(val instanceof RegExp) info = new JSRegExp(val.source, val.flags);
       else if(isArray || isTypedArray) info = new JSArray(isTypedArray ? null : [...val].map(v => EncodeJS(v)));
       else info = new JSObject();
-
       const proto = Object.getPrototypeOf(val);
       const tag = (proto ? proto[Symbol.toStringTag] : val[Symbol.toStringTag]) ?? (proto != Object.prototype && val?.constructor?.name);
       if(tag) info.class = tag;
-
       if(isArrayBuffer) info.data = [...new Uint8Array(val)].reduce((a, n) => (a ? a + ',' : '') + n.toString(16).padStart(2, '0'), '');
-
       if(!isFunction) {
         const members = isArray || isTypedArray ? [...val].map(i => EncodeJS(i)) : EncodeObj(val);
         if(!isTypedArray) {
@@ -63,42 +61,34 @@ export function EncodeJS(val, info = Object.setPrototypeOf({}, null)) {
         }
         if(!(isArray || isTypedArray)) if (proto) define(info, { proto: EncodeJS(proto) });
       }
-
       break;
-    }
     case 'number':
     case 'bigfloat':
     case 'bigint':
-    case 'bigdecimal': {
+    case 'bigdecimal':
       info = new JSNumber(undefined, type == 'number' ? undefined : type);
       info.value = val + '';
       break;
-    }
-    case 'string': {
+    case 'string':
       info = new JSString(val);
       break;
-    }
-    case 'boolean': {
+    case 'boolean':
       info = new JSBoolean(val + '');
       break;
-    }
-    case 'undefined': {
+    case 'undefined':
       info = new JSValue();
       info.type = type;
       break;
-    }
-    case 'symbol': {
+    case 'symbol':
       const str = val.toString();
       if(/Symbol\(Symbol\.([^)]*)\)/.test(str)) info = new JSSymbol(undefined, str.replace(/Symbol\(Symbol\.([^)]*)\)/g, '$1'));
       else info = new JSSymbol(str.replace(/Symbol\(([^)]*)\)/g, '$1'));
       break;
-    }
-    default: {
+    default:
       info = new JSValue();
       info.type = type;
       info.value = val + '';
       break;
-    }
   }
 
   return info;
@@ -106,27 +96,18 @@ export function EncodeJS(val, info = Object.setPrototypeOf({}, null)) {
 
 export function EncodeObj(obj) {
   const members = Object.setPrototypeOf({}, null);
-
-  try {
-    const props = Object.getOwnPropertyDescriptors(obj);
-
-    for(let k in props) {
-      const a = props[k];
-      let r;
-      if(a.get || a.set) r = new JSProperty(a.get && (a.get + '').replace(/^[^{]*{/, '() => {'), a.set && (a.set + '').replace(/^[^{]*{/, '() => {'));
-      else r = EncodeJS(obj[k]);
-      if(a.enumerable === false) define(r, { enumerable: a.enumerable });
-      if(a.configurable === false) define(r, { configurable: a.configurable });
-      if(!('value' in a) && 'writable' in a) define(r, { writable: a.writable });
-
-      members[k] = r;
-    }
-
-    return members;
-  } catch(e) {
-    console.log('error', { e, obj });
-    throw e;
+  const props = Object.getOwnPropertyDescriptors(obj);
+  for(let k in props) {
+    const a = props[k];
+    let r;
+    if(a.get || a.set) r = new JSProperty(a.get && (a.get + '').replace(/^[^{]*{/, '() => {'), a.set && (a.set + '').replace(/^[^{]*{/, '() => {'));
+    else r = EncodeJS(obj[k]);
+    if(a.enumerable === false) define(r, { enumerable: a.enumerable });
+    if(a.configurable === false) define(r, { configurable: a.configurable });
+    if(!('value' in a) && 'writable' in a) define(r, { writable: a.writable });
+    members[k] = r;
   }
+  return members;
 }
 
 export function DecodeJS(info) {
@@ -135,7 +116,7 @@ export function DecodeJS(info) {
   switch (info.type) {
     case 'array':
     case 'function':
-    case 'object': {
+    case 'object':
       if(info.type == 'function')
         try {
           val = eval(info.code);
@@ -143,14 +124,11 @@ export function DecodeJS(info) {
           console.log('eval', info.code);
         }
       else if(info.type == 'array') {
-        const { buffer, ...members } = info.members ?? {};
-        if(buffer && info.class) {
-          const b = DecodeJS(buffer);
-          val = new globalThis[info.class](b);
-        } else if(Array.isArray(members)) {
-          val = [...members].map(i => DecodeJS(i));
-        }
-        break;
+        if(info.class && info.class != 'Array' && info.members.buffer) {
+          const { buffer, ...members } = info.members;
+          info.members = members;
+          val = new globalThis[info.class](DecodeJS(buffer));
+        } else if(Array.isArray(info.members)) val = [...info.members].map(i => DecodeJS(i));
       } else if(info.class == 'ArrayBuffer') {
         val = new Uint8Array(info.data.split(',').map(s => parseInt(s, 16))).buffer;
       } else val = {};
@@ -173,51 +151,39 @@ export function DecodeJS(info) {
         Object.defineProperties(val, props);
       }
       break;
-    }
-    case 'boolean': {
+    case 'boolean':
       val = info.value == 'true' ? true : false;
       break;
-    }
-    case 'bigint': {
+    case 'bigint':
       val = BigInt(info.value);
       break;
-    }
-    case 'bigfloat': {
+    case 'bigfloat':
       val = BigFloat(info.value);
       break;
-    }
-    case 'bigdecimal': {
+    case 'bigdecimal':
       val = BigDecimal(info.value);
       break;
-    }
-    case 'number': {
+    case 'number':
       val = Number(info.value);
       break;
-    }
-    case 'string': {
+    case 'string':
       val = info.value + '';
       break;
-    }
-    case 'regexp': {
+    case 'regexp':
       val = new RegExp(info.source, info.flags);
       break;
-    }
-    case 'symbol': {
+    case 'symbol':
       val = info.global ? Symbol[info.global] : Symbol.for(info.value);
       break;
-    }
-    case 'undefined': {
+    case 'undefined':
       val = undefined;
       break;
-    }
-    case 'null': {
+    case 'null':
       val = null;
       break;
-    }
-    default: {
+    default:
       val = info.value;
       break;
-    }
   }
 
   return val;
